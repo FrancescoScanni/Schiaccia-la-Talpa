4f8cf6e2bd41c8cecf86c973915fb20dc0c809c8
FrancescoScanni <frascanni07@gmail.com>
2024-12-30 02:18:08 +0100

    Talpa (v1.7 comparsa talpe + score)

--- static/js/index.js
+++ static/js/index.js
@@ -1,25 +1,71 @@
 
 let button=document.querySelector("#button")
+let punti=document.querySelector("#punti")
 let mole=document.querySelectorAll(".mole")
+let tana=document.querySelectorAll(".den")
 
-
+let finito
+let score
+let max
+let min
+let index=generateRandomIndex()
 
 function generateRandomIndex(){
     return Math.floor(Math.random()*9)   
 }
-function generateRandomTime(){
-    return (Math.floor(Math.random()*4)+1)*1000   
+function generateRandomTime(max,min){
+    return Math.floor(Math.random()*(max-min)+min)  
 }
 
-function show(){
-    let index=generateRandomIndex()
-    mole[index].classList.add("hit")
+function show() {
+    index = generateRandomIndex();
+    let time = generateRandomTime(700, 1700);
+
+    mole[index].addEventListener("click", hit);
+    mole[index].classList.add("hit");
+
+
+    setTimeout(function () {
+        mole[index].classList.remove("hit");
+        mole[index].removeEventListener("click", hit);
+
+        if (!finito) {
+            show();
+        } else {
+            button.style.visibility = "visible";
+        }
+    }, time);
+}
+
+function hit() {
+    mole[index].classList.remove("hit");
+    score++;
+    punti.innerHTML = score;    
+    
+    console.log(score);
+}
+
+
+
+function start(){
+    button.style.visibility="hidden"
+    finito=false
+    score=0
+    punti.innerHTML="-"
+    show()
     setTimeout(function(){
-        mole[index].classList.remove("hit")
-    },500)
+        for(let i=0;i<mole.length;i++){
+            mole[i].removeEventListener("click",hit)   //rimozione eventlistener in modo da non avere una talpa ancora cliccabile quando termina il tempo
+        }
+        finito=true
+        
+    },10000)
+    
 }
 
 
 
-button.addEventListener("click",show)
+//MAIN
+
+button.addEventListener("click",start)
     
