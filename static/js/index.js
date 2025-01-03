9ad7fef48fa0e81120dc27caab8711d0528233ff
FrancescoScanni <frascanni07@gmail.com>
2025-01-03 16:47:54 +0100

    Talpa (v2.1 Aggiunta sfondi e suoni alle interazioni col gioco)

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,8 @@
 let sec_bg=document.querySelector("#sec-layer")
 let cup=document.querySelector("#cup")
+let winner=document.querySelector("#winner")
+let gameover=document.querySelector("#gameover")
+let loser=document.querySelector("#loser")
 let button=document.querySelector("#button")
 let punti=document.querySelector("#punti")
 let timer=document.querySelector("#cronometro")
@@ -9,6 +12,13 @@ let mole=document.querySelectorAll(".mole")
 let bomb=document.querySelectorAll(".bomb")
 let tana=document.querySelectorAll(".den")
 
+//variabili della classe Audio per ripodure i suoni in determinati momeni
+const colpita=new Audio("static/audio/hit.mp3")
+const esplosione=new Audio("static/audio/boom.mp3")
+const vittoria=new Audio("static/audio/victory.mp3")
+const sconfitta=new Audio("static/audio/gameover.mp3")
+
+//variabili globali
 const durata=20000  //durata complessiva gioco (si può cambiare tutto  modificando la durata mas olo da questa riga )
 let finito //booleana fine/inizio gioco
 let score  //punteggio talpe prese
@@ -56,10 +66,12 @@ function show() {
             show()
         } else {
             if(score===5){
+                sec_bg.classList.add("additionl-bg-win")
+                vittoria.play()
+                winner.style.visibility="visible"
                 cup.style.visibility="visible"
             }
             button.style.visibility = "visible"
-
         }
     }, time);
 }
@@ -92,6 +104,7 @@ function showBomb(){
 
 //iterazioni al tocco bomba/talpa
 function hit() {
+    colpita.play()
     right=right+1
     stars[right].classList.remove("is-transparent") 
     mole[index].classList.remove("hit")
@@ -112,6 +125,7 @@ function hit() {
     
 }
 function explode(){
+    esplosione.play()
     err=err+1
     life[err].classList.add("is-empty") 
     bomb[indexB].removeEventListener("click",explode)
@@ -121,18 +135,36 @@ function explode(){
     console.log("boom")
       //riduci vita
 
-    sec_bg.style.opacity="1"
-    sec_bg.classList.add("additionl-bg-err")   //sfondo verde che indica punto
-    setTimeout(function(){
-        sec_bg.classList.remove("additionl-bg-err")
-    },500)
+    if(err===2){
+        //sconfitta
+        sconfitta.play()
+        sec_bg.classList.add("additionl-bg-lose")
+        loser.style.visibility="visible"
+        gameover.style.visibility="visible"
+        finito=true
+    }else{
+        sec_bg.style.opacity="1"
+        sec_bg.classList.add("additionl-bg-err")   //sfondo che indica -1 vita
+        setTimeout(function(){
+            sec_bg.classList.remove("additionl-bg-err")
+        },500)
+    }
 }
 
 
 //inizio gioco
 function start(){
+    //blocchi annullamento inizio gioco
+    
+    sec_bg.classList.remove("additionl-bg-win")
     cup.style.visibility="hidden"
-    timer.value=durata  //il tempo e si resetta
+    winner.style.visibility="hidden"
+    sec_bg.classList.remove("additionl-bg-lose")
+    gameover.style.visibility="hidden"
+    loser.style.visibility="hidden"
+
+    //il tempo e si resetta e riprende il tutto
+    timer.value=durata  
     timer.max=durata
     let intervallo_timer = setInterval(function(){
         if (!finito){
@@ -174,6 +206,8 @@ function start(){
 //-----------------------------------------------MAIN-----------------------------------------------
 
 button.addEventListener("click",start)
+
+
 
 
 
